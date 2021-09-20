ca6ac6b298d9bc51ab81e5bb8c2ce6bb7684b207
Peter Svensson <peter@sparetimecoders.com>
2021-09-20 21:11:59 +0200

    doc

--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,12 @@ output "private_subnets" {
   value = tomap({
     for k, v in aws_subnet.private : k => v.id
   })
+  description = "The private networks created as a map <zone> => <subnet id>"
 }
 
 output "public_subnets" {
   value = tomap({
     for k, v in aws_subnet.public : k => v.id
   })
+  description = "The public networks created as a map <zone> => <subnet id>"
 }
