751b34d197e06edece49e892d1499f044acee16f
Peter Svensson <peter@sparetimecoders.com>
2021-09-20 21:11:55 +0200

    CIDR range fix

--- locals.tf
+++ locals.tf
@@ -2,6 +2,6 @@ locals {
   public_cidrs = length(var.private_subnet_zones) > 0 ? tomap({ for i, zone in var.private_subnet_zones : zone => cidrsubnet(var.vpc_cidr, 3, i) }) : tomap({ for i, zone in var.public_subnet_zones : zone => cidrsubnet(var.vpc_cidr, 3, i) })
 
   private_cidrs = tomap({
-    for i, zone in var.private_subnet_zones : zone => cidrsubnet(var.vpc_cidr, 3, length(var.private_subnet_zones) + i)
+    for i, zone in var.private_subnet_zones : zone => cidrsubnet(var.vpc_cidr, 3, length(local.public_cidrs) + i)
   })
 }
