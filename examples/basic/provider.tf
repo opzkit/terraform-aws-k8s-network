fe8129e3202c0856f98235228213699db9756cba
Peter Svensson <peter@sparetimecoders.com>
2022-10-17 09:05:29 +0200

    chore: change deprecated s3_force_path_style to s3_use_path_style

--- examples/basic/provider.tf
+++ examples/basic/provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   skip_requesting_account_id  = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
-  s3_force_path_style         = true
+  s3_use_path_style           = true
   region                      = "eu-west-1"
   access_key                  = "mock_access_key"
   secret_key                  = "mock_secret_key"
@@ -12,7 +12,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
 }
